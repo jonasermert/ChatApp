059136ab1a801a1dd6cb7281071b3c2cd799eada
ermjonertas <ermert.jonas@yahoo.de>
2023-09-06 04:21:57 +0200

    Firebase Setup

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -10,7 +10,6 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  
   final _form = GlobalKey<FormState>();
 
   var _isLogin = true;
@@ -52,58 +51,61 @@ class _AuthScreenState extends State<AuthScreen> {
                     child: Padding(
                   padding: const EdgeInsets.all(16),
                   child: Form(
-                    key: _form,
-                    child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      TextFormField(
-                        decoration: const InputDecoration(labelText: 'Email'),
-                        keyboardType: TextInputType.emailAddress,
-                        autocorrect: false,
-                        textCapitalization: TextCapitalization.none,
-                        validator: (value) {
-                          if (value == null || value.trim().isEmpty || !value.contains('@')) {
-                            return 'Please enter a valid email adress';
-                          }
+                      key: _form,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Email'),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  !value.contains('@')) {
+                                return 'Please enter a valid email adress';
+                              }
 
-                          return null;
-                        },
-                        onSaved: (value) {
-                          _enteredEmail = value!;
-                        },
-                      ),
-                      TextFormField(
-                        decoration:
-                            const InputDecoration(labelText: 'Password'),
-                        obscureText: true,
-                        validator: (value) {
-                          if (value == null || value.trim().length < 6) {
-                            return 'Password must be at least 6 characters long';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                          _enteredPassword = value!;
-                        },
-                      ),
-                      const SizedBox(height: 12),
-                      ElevatedButton(
-                        onPressed: _submit,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Theme.of(context)
-                          .colorScheme
-                          .primaryContainer,
-                        )
-                        child: Text(_isLogin ? 'Login' : 'SignUp'),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          _isLogin = !_isLogin;
-                        },
-                        child: Text(_isLogin ? 'Create Account' : 'Login'),
-                      )
-                    ],
-                  )),
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _enteredEmail = value!;
+                            },
+                          ),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Password'),
+                            obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.trim().length < 6) {
+                                return 'Password must be at least 6 characters long';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _enteredPassword = value!;
+                            },
+                          ),
+                          const SizedBox(height: 12),
+                          ElevatedButton(
+                            onPressed: _submit,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
+                            ),
+                            child: Text(_isLogin ? 'Login' : 'SignUp'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              _isLogin = !_isLogin;
+                            },
+                            child: Text(_isLogin ? 'Create Account' : 'Login'),
+                          )
+                        ],
+                      )),
                 )),
               ),
             ],
